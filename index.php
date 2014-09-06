48a71fa808ac7d53c64905bf8da76e117e6811c8
mzm-dev <zakimustafa@Mohamads-MacBook-Pro.local>
2014-09-06 11:35:37 +0800

    modified

---
+++ index.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ *
+ *
+ * @link          https://github.com/mzm-dev 
+ * @created       September 2014
+ * @fb            https://www.facebook.com/zakimedia
+ * @email         mohdzaki04@gmail.com
+ */
+$cakeDescription = "Highcharts Chart PHP with MySQL Example";
+?>
+<!DOCTYPE html>
+<html>
+    <head>	
+        <title><?php echo $cakeDescription ?></title>
+        <link href="webroot/css/cake.generic.css" media="screen" type="text/css" rel="stylesheet">
+    </head>
+    <body>        
+        <div id="container">
+            <div id="header">
+                <h1><?php echo $cakeDescription; ?></h1>
+            </div>
+            <div id="content">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>PIE CHARTS</th>                            
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td><a href="pie/">Pie Chart</a></td>                          
+                        </tr>                       
+                        <tr>                          
+                            <td><a href="#">Column(coming)</a></td>                            
+                        </tr>                       
+                    </tbody>
+                </table>
+
+                <p>
+                    Get the code on Github now <a href="https://github.com/pesima/highcharts">HighCharts</a>          
+                </p>
+            </div>
+            <div id="footer">
+                <a target="_blank" href="https://github.com/pesima/highcharts">MZM-DEV : pesima/highcharts</a>		
+            </div>
+        </div>      
+    </body>
+</html>
--- pie/index.php
+++ pie/index.php
@@ -1,8 +1,20 @@
+<?php
+/**
+ *
+ *
+ * @link          https://github.com/mzm-dev 
+ * @created       September 2014
+ * @fb            https://www.facebook.com/zakimedia
+ * @email         mohdzaki04@gmail.com
+ */
+$cakeDescription = "Highcharts Pie Chart";
+?>
 <!DOCTYPE HTML>
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-        <title>Highcharts Pie Chart</title>
+        <title><?php echo $cakeDescription ?></title>
+        <link href="../webroot/css/cake.generic.css" type="text/css" rel="stylesheet">
         <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
         <script type="text/javascript">
             $(document).ready(function() {
@@ -18,7 +30,7 @@
                     },
                     tooltip: {
                         formatter: function() {
-                            return '<b>' + this.point.name + '</b>: ' + this.percentage + ' %';
+                            return '<b>' + this.point.name + '</b>: ' + this.y;
                         }
                     },
                     plotOptions: {
@@ -30,30 +42,31 @@
                                 color: '#000000',
                                 connectorColor: '#000000',
                                 formatter: function() {
-                                    return '<b>' + this.point.name + '</b>: ' + this.percentage + ' %';
+                                    return '<b>' + this.point.name + '</b>: ' + this.y;
                                 }
-                            }
+                            },
+                            showInLegend: true
                         }
                     },
                     series: []
-                    //Bentuk data sebenar
-                    /*series: [{
-                            "type":"pie",
-                            "name":"Revenue",
-                            "data":[
-                                ["Direct Sales",20],
-                                ["Search Engine Marketing",15],
-                                ["PPC Advertising",15],
-                                ["Website Marketing",10],
-                                ["Blog Marketing",10],
-                                ["Social Media Marketing",10],
-                                ["Email Marketing",10],
-                                ["Online PR",2.5],
-                                ["Multimedia Marketing",2.5],
-                                ["Mobile Marketing",2.5],
-                                ["Display Advertising",2.5]
-                            ]
-                        }]*/
+                            //Bentuk data sebenar
+                            /*series: [{
+                             "type":"pie",
+                             "name":"Revenue",
+                             "data":[
+                             ["Direct Sales",20],
+                             ["Search Engine Marketing",15],
+                             ["PPC Advertising",15],
+                             ["Website Marketing",10],
+                             ["Blog Marketing",10],
+                             ["Social Media Marketing",10],
+                             ["Email Marketing",10],
+                             ["Online PR",2.5],
+                             ["Multimedia Marketing",2.5],
+                             ["Mobile Marketing",2.5],
+                             ["Display Advertising",2.5]
+                             ]
+                             }]*/
                 }
 
                 $.getJSON("data.php", function(json) {
